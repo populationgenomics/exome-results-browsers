abdab1efb2ca3fdfe5738a1fde9cc611d082e722
daniaki <7043686+daniaki@users.noreply.github.com>
2021-07-21 19:15:48 +1000

    Added resovleFile to interface

--- src/server/storage/IDataStore.js
+++ src/server/storage/IDataStore.js
@@ -1,9 +1,23 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable class-methods-use-this */
+const path = require('path')
+
 class IDataStore {
   constructor(rootDirectory) {
     this.rootDirectory = rootDirectory
   }
 
-  read
+  /**
+   * Returns the location of a file on local file system.
+   *
+   * @param {string} fileName
+   * @param {string, null} subdirectory
+   *
+   * @returns {Promise<string>}
+   */
+  resolveFile(fileName, subdirectory = null) {
+    throw new Error('Method has not been implemented')
+  }
 }
 
 module.exports = { IDataStore }
