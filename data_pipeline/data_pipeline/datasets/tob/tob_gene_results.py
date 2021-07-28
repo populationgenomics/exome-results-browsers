cc9d978000bb6a69a957aec83dd4262257d8ff79
daniaki <7043686+daniaki@users.noreply.github.com>
2021-07-28 18:05:14 +1000

    Black formatting

--- data_pipeline/data_pipeline/datasets/tob/tob_gene_results.py
+++ data_pipeline/data_pipeline/datasets/tob/tob_gene_results.py
@@ -1,7 +1,7 @@
 import hail as hl
 
-from data_pipeline.config import pipeline_config
+# from data_pipeline.config import pipeline_config
 
 
 def prepare_gene_results() -> hl.Table:
-    pass    
+    pass
--- data_pipeline/data_pipeline/datasets/tob/tob_variant_results.py
+++ data_pipeline/data_pipeline/datasets/tob/tob_variant_results.py
@@ -1,6 +1,6 @@
 import hail as hl
 
-from data_pipeline.config import pipeline_config
+# from data_pipeline.config import pipeline_config
 
 
 def prepare_variant_results() -> hl.Table:
