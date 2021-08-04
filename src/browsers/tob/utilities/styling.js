fe100f4583655ee07a8208d95c2dd030f8e82c22
daniaki <7043686+daniaki@users.noreply.github.com>
2021-08-04 16:49:52 +1000

    Common utilities

---
+++ src/browsers/tob/utilities/styling.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const TooltipWrapper = styled.div`
+  margin: 1em 0;
+`
+
+export const PlotWrapper = styled.div`
+  margin: 1em 0;
+`
+
+export default {
+  TooltipWrapper,
+  PlotWrapper,
+}
