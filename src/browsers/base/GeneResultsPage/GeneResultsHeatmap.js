010d7caca933956555057da05beb39377182a16b
daniaki <7043686+daniaki@users.noreply.github.com>
2021-08-11 14:13:55 +1000

    Added linear gradient scale, resizing fix, updated props

--- src/browsers/base/GeneResultsPage/GeneResultsHeatmap.js
+++ src/browsers/base/GeneResultsPage/GeneResultsHeatmap.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import { minBy, maxBy } from 'lodash'
 import { withSize } from 'react-sizeme'
 import {
   scaleBand,
@@ -15,13 +16,15 @@ import {
 const margin = { left: 80, right: 80, top: 40, bottom: 80 }
 
 const GeneResultsHeatmap = ({
+  rowLabels,
+  columnLabels,
+  data,
+  onClickTile,
+  onHoverTile,
+  renderTooltip,
+  id,
   height,
   width,
-  id,
-  onClickCell,
-  columnLabels,
-  rowLabels,
-  cellData,
   minValue,
   minValueColor,
   maxValue,
@@ -30,19 +33,15 @@ const GeneResultsHeatmap = ({
   tileSelectBorderColor,
   tileSelectBorderWidth,
 }) => {
-  const newCellData = cellData
-    .map((d, i) => {
-      return d.map((e, j) => {
-        return { xValue: columnLabels[j], yValue: rowLabels[i], pValue: e }
-      })
-    })
-    .flat()
   const svgRef = useRef()
   const wrapperRef = useRef()
 
   useEffect(() => {
     const svg = select(svgRef.current)
     const svgContent = svg.select('.content')
+
+    const minScaleValue = minValue == null ? minBy(data, (d) => d.value).value : minValue
+    const maxScaleValue = maxValue == null ? maxBy(data, (d) => d.value).value : maxValue
 
     const xScale = scaleBand()
       .range([margin.left, width - margin.right])
@@ -53,82 +52,96 @@ const GeneResultsHeatmap = ({
       .domain(rowLabels)
       .padding(tileSpacing)
 
-    const legendScale = scaleLinear().domain([minValue, maxValue])
-
-    const colourScale = scaleSequential()
+    const colorScale = scaleSequential()
       .interpolator(interpolateRgb(minValueColor, maxValueColor))
-      .domain([minValue, maxValue])
+      .domain([minScaleValue, maxScaleValue])
+      .range([minValueColor, maxValueColor])
 
     const Tooltip = select(wrapperRef.current).select('.tooltip')
 
     Tooltip.style('opacity', 0)
-      .attr('class', 'tooltip')
+      .attr('class', 'tooltip-active')
       .style('background-color', 'white')
       .style('border', 'solid')
-      .style('border-width', '2px')
+      .style('border-width', '1px')
       .style('border-radius', '5px')
       .style('padding', '5px')
       .style('position', 'absolute')
 
-    const mouseover = function MouseOver() {
+    function mouseOver() {
       Tooltip.style('opacity', 1)
       select(this)
         .style('stroke', tileSelectBorderColor)
         .style('stroke-width', `${tileSelectBorderWidth}`)
         .style('opacity', 1)
     }
-    const mousemove = function mouseMove(e, d) {
-      Tooltip.html(+d.pValue.toFixed(10))
+    function mouseMove(_, d) {
+      onHoverTile(d)
+      Tooltip.html(renderTooltip(d))
         .style('opacity', 1)
-        .style('left', `${xScale(d.xValue)}px`)
-        .style('top', `${yScale(d.yValue) - 30}px`)
+        .style('left', `${xScale(d.col)}px`)
+        .style('top', `${yScale(d.row) - 30}px`)
     }
-    const mouseleave = function MouseLeave() {
+    function mouseLeave() {
       Tooltip.style('opacity', 0)
       select(this).style('stroke', 'none').style('opacity', 0.8)
     }
+    function mouseClick(_, d) {
+      onClickTile(d)
+    }
 
+    // Clear content for re-draw so rectangles aren't drawn over each other.
+    svgContent.selectAll('rect').remove()
     svgContent
       .selectAll()
-      .data(newCellData, (d) => `${d.xValue}:${d.yValue}`)
+      .data(data, (d) => `${d.col}:${d.row}`)
       .enter()
       .append('rect')
-      .attr('x', (d) => xScale(d.xValue))
-      .attr('y', (d) => yScale(d.yValue))
+      .attr('x', (d) => xScale(d.col))
+      .attr('y', (d) => yScale(d.row))
       .attr('width', xScale.bandwidth())
       .attr('height', yScale.bandwidth())
-      .style('fill', (d) => colourScale(d.pValue))
+      .style('fill', (d) => colorScale(d.value))
       .style('stroke-width', 4)
       .style('stroke', 'none')
       .style('opacity', 0.8)
-      .on('mouseover', mouseover)
-      .on('mousemove', mousemove)
-      .on('mouseleave', mouseleave)
+      .on('mouseover', mouseOver)
+      .on('mousemove', mouseMove)
+      .on('mouseleave', mouseLeave)
+      .on('click', mouseClick)
 
-    const legend = svg
-      .select('.legend')
-      .selectAll()
-      .data(legendScale.ticks())
-      .enter()
-      .append('g')
-      .attr(
-        'transform',
-        (d, i) => `translate(${width - margin.right + 10}, ${margin.top + i * 20})`
-      )
+    // Update the legend gradient def
+    svg.select('#grad-start').attr('stop-color', colorScale(minScaleValue))
+    svg.select('#grad-stop').attr('stop-color', colorScale(maxScaleValue))
 
-    legend
-      .append('rect')
+    svg
+      .select('.legend-content')
+      .attr('x', width - margin.right + 8)
+      .attr('y', margin.top)
       .attr('width', 40)
-      .attr('height', 20)
-      .attr('fill', (d) => colourScale(legendScale(d)))
+      .attr('height', height - margin.bottom * 1.5)
+      .style('fill', 'url(#grad)')
 
-    legend
-      .append('text')
+    svg
+      .select('.legend-content-min-label')
       .attr('font-size', 10)
-      .attr('x', 45)
-      .attr('y', 10)
-      .text(String)
-      .attr('alignment-baseline', 'middle')
+      .attr('x', width - margin.right + 40 + 10)
+      .attr('y', height - margin.bottom)
+      .text(minScaleValue)
+
+    svg
+      .select('.legend-content-mid-label')
+      .attr('font-size', 10)
+      .attr('x', width - margin.right + 40 + 10)
+      .attr('y', (height - margin.bottom) / 2)
+      .text(((minScaleValue + maxScaleValue) / 2).toString())
+
+    svg
+      .select('.legend-content-max-label')
+      .attr('font-size', 10)
+      .attr('x', width - margin.right + 40 + 10)
+      .attr('y', margin.top + 10)
+      .text(maxScaleValue.toString())
 
     const xAxis = axisBottom(xScale)
     svg
@@ -138,7 +151,24 @@ const GeneResultsHeatmap = ({
 
     const yAxis = axisLeft(yScale)
     svg.select('.y-axis').attr('transform', `translate(${margin.left}, 0)`).call(yAxis)
-  }, [cellData, columnLabels, rowLabels, onClickCell])
+  }, [
+    rowLabels,
+    columnLabels,
+    data,
+    onClickTile,
+    onHoverTile,
+    renderTooltip,
+    id,
+    height,
+    width,
+    minValue,
+    minValueColor,
+    maxValue,
+    maxValueColor,
+    tileSpacing,
+    tileSelectBorderColor,
+    tileSelectBorderWidth,
+  ])
 
   return (
     <>
@@ -148,11 +178,20 @@ const GeneResultsHeatmap = ({
             <clipPath id={id}>
               <rect x="0" y="0" width="100%" height="100%" />
             </clipPath>
+            <linearGradient id="grad" x1="0%" x2="0%" y1="100%" y2="0%">
+              <stop id="grad-start" offset="0%" />
+              <stop id="grad-stop" offset="100%" />
+            </linearGradient>
           </defs>
           <g className="content" clipPath={`url(#${id})`} />
           <g className="x-axis" />
           <g className="y-axis" />
-          <g className="legend" />
+          <g className="legend">
+            <rect className="legend-content" />
+            <text className="legend-content-min-label" />
+            <text className="legend-content-mid-label" />
+            <text className="legend-content-max-label" />
+          </g>
         </svg>
         <div className="tooltip" />
       </div>
@@ -161,17 +200,25 @@ const GeneResultsHeatmap = ({
 }
 
 GeneResultsHeatmap.propTypes = {
+  columnLabels: PropTypes.arrayOf(PropTypes.string).isRequired,
+  rowLabels: PropTypes.arrayOf(PropTypes.string).isRequired,
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      row: PropTypes.string.isRequired,
+      col: PropTypes.string.isRequired,
+      value: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+  id: PropTypes.string,
   height: PropTypes.number,
   width: PropTypes.number,
-  id: PropTypes.string,
   minValue: PropTypes.number,
   maxValue: PropTypes.number,
   minValueColor: PropTypes.string,
   maxValueColor: PropTypes.string,
-  onClickCell: PropTypes.func.isRequired,
-  columnLabels: PropTypes.arrayOf(PropTypes.string).isRequired,
-  rowLabels: PropTypes.arrayOf(PropTypes.string).isRequired,
-  cellData: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.number)).isRequired,
+  onClickTile: PropTypes.func,
+  onHoverTile: PropTypes.func,
+  renderTooltip: PropTypes.func,
   tileSpacing: PropTypes.number,
   tileSelectBorderWidth: PropTypes.number,
   tileSelectBorderColor: PropTypes.string,
@@ -181,23 +228,26 @@ GeneResultsHeatmap.defaultProps = {
   height: 800,
   width: 500,
   id: 'gene-results-heatmap',
-  minValue: 0,
-  maxValue: 1,
+  minValue: null,
+  maxValue: null,
   minValueColor: '#FFFFFF',
   maxValueColor: '#6C010E',
   tileSpacing: 0,
-  tileSelectBorderWidth: 1,
+  tileSelectBorderWidth: 4,
   tileSelectBorderColor: '#FF0000',
+  onClickTile: () => {},
+  onHoverTile: () => {},
+  renderTooltip: (d) => d.value.toFixed(4),
 }
 
 const Wrapper = styled.div`
-  overflow: hidden;
+  overflow: visible;
   width: 100%;
 `
 
 const AutosizedGeneResultsHeatmap = withSize()(({ size, ...otherProps }) => (
   <Wrapper>
-    {Boolean(size.width) && <GeneResultsHeatmap height={800} width={size.width} {...otherProps} />}
+    {Boolean(size.width) && <GeneResultsHeatmap width={size.width} {...otherProps} />}
   </Wrapper>
 ))
 
