47293e6d00d92b1ea01fb5f7c0c250e059acb0fb
daniaki <7043686+daniaki@users.noreply.github.com>
2021-08-12 13:07:10 +1000

    Stubs for state management

--- src/browsers/tob/TOBHeatmapLocusPlot.js
+++ src/browsers/tob/TOBHeatmapLocusPlot.js
@@ -5,84 +5,137 @@ import { SearchInput } from '@gnomad/ui'
 
 import Fetch from '../base/Fetch'
 import StatusMessage from '../base/StatusMessage'
-import AutosizedGeneResultsHeatmap from '../base/GeneResultsPage/GeneResultsHeatmap'
+import {
+  AutosizedGeneResultsHeatmap,
+  TileEventType,
+} from '../base/GeneResultsPage/GeneResultsHeatmap'
 import AutosizedGeneResultsManhattanPlot from '../base/GeneResultsPage/GeneResultsManhattanPlot'
 
 const TOBHeatmapLocusPlot = () => {
-  const [apiPath, setApiPath] = useState('/heatmap/')
+  // ===============================================================================================
+  // Heatmap state management
+  // ===============================================================================================
   const [searchText, setSearchText] = useState('')
-  const [requestParams, setRequestParams] = useState({ search: '' })
+  const [heatmapApiPath, setHeatmapApiPath] = useState('/heatmap/')
+  const [heatmapRequestParams, setheatmapRequestParams] = useState({ search: '', pValMax: null })
 
-  useEffect(() => {
-    if (requestParams.search?.trim()?.length > 0) {
-      setApiPath(`/heatmap/?search=${requestParams.search.trim()}`)
-    }
-  }, [requestParams])
-
-  const debounceSetRequestParams = useCallback(
-    debounce((value) => setRequestParams(value), 1000),
+  const debounceSetHeatmapRequestParams = useCallback(
+    debounce((value) => setheatmapRequestParams(value), 1000),
     []
   )
+
+  /**
+   * @param {string} value
+   */
   const handleSearchInputChange = (value) => {
     setSearchText(value)
-    debounceSetRequestParams({ ...requestParams, search: value?.trim() })
+    if (value?.trim()) {
+      debounceSetHeatmapRequestParams({ ...heatmapRequestParams, search: value.trim() }, 1000)
+    }
   }
 
+  useEffect(() => {
+    setHeatmapApiPath(`/heatmap/?${new URLSearchParams(heatmapRequestParams).toString()}`)
+  }, [heatmapRequestParams])
+
+  // ===============================================================================================
+  // Locus plot state management
+  // ===============================================================================================
+  // Keep track of selected heatmap tiles and which ones we have already fetched locus plot data for
+  const [selectedTiles, setSelectedTiles] = useState(new Map())
+  const [locusPlotApiPath, setLocusPlotApiPath] = useState('/locus-plot/')
+  const [locusPlotRequestParams, setLocusPlotRequestParams] = useState({
+    geneCellPairs: [
+      ['SELM', 'MonoC'],
+      ['NDUFA6', 'NKact'],
+    ],
+    pValMax: null,
+  })
+
+  const debounceSetLocusPlotRequestParams = useCallback(
+    debounce((value) => setLocusPlotRequestParams(value), 1000),
+    []
+  )
+
+  /**
+   * @param {{row: string, col: string, value: number}} tile
+   * @param {TileEventType} eventType
+   */
+  const handleTileClick = (tile, eventType) => {
+    console.log(`${eventType} tile Tile(${tile.row}, ${tile.col}, ${tile.value})`)
+  }
+
+  useEffect(() => {
+    const params = new URLSearchParams({ delimiter: '|' })
+
+    if (locusPlotRequestParams.pValMax != null) {
+      params.append('pValMax', locusPlotRequestParams.pValMax)
+    }
+    if (locusPlotRequestParams.geneCellPairs.length > 0) {
+      locusPlotRequestParams.geneCellPairs.forEach((pair) => {
+        params.append('geneCellPair', pair.join('|'))
+      })
+    }
+
+    setLocusPlotApiPath(`/locus-plot/?${params.toString()}`)
+  }, [locusPlotRequestParams])
+
+  // ===============================================================================================
+  // Rendering
+  // ===============================================================================================
   return (
     <>
-      <Fetch path="/locus-plot" params>
-        {({ data, error, loading }) => {
-          if (loading) {
-            return <StatusMessage>Loading locus-plot...</StatusMessage>
-          }
+      <SearchInput
+        id="heatmap-search"
+        placeholder="rs45448095, 1-55039774-C-T, 1:55505221-55530525"
+        value={searchText}
+        onChange={handleSearchInputChange}
+      />
+      <div style={{ margin: '1em 0' }}>
+        <Fetch path={locusPlotApiPath}>
+          {({ data, error, loading }) => {
+            if (loading) {
+              return <StatusMessage>Loading locus plot...</StatusMessage>
+            }
 
-          if (error || !(data || {}).results) {
-            return <StatusMessage>Unable to load locus-plot</StatusMessage>
-          }
+            if (error || !(data || {}).results) {
+              return <StatusMessage>Unable to load locus plot</StatusMessage>
+            }
 
-          return (
-            <>
-              <SearchInput
-                id="heatmap-search"
-                placeholder="rs45448095, 1-55039774-C-T, 1:55505221-55530525"
-                isDisabled={loading || error}
-                value={searchText}
-                onChange={handleSearchInputChange}
+            return <AutosizedGeneResultsManhattanPlot results={data.results.data} />
+          }}
+        </Fetch>
+      </div>
+      <div style={{ margin: '1em 0' }}>
+        <Fetch path={heatmapApiPath}>
+          {({ data, error, loading }) => {
+            if (loading) {
+              return <StatusMessage>Loading heatmap...</StatusMessage>
+            }
+
+            if (error || !(data || {}).results) {
+              return <StatusMessage>Unable to load heatmap</StatusMessage>
+            }
+
+            return (
+              <AutosizedGeneResultsHeatmap
+                id="gene-results-heatmap"
+                height={1200}
+                columnLabels={data.results.columnLabels}
+                rowLabels={data.results.rowLabels}
+                data={data.results.data}
+                minValue={data.results.minValue}
+                maxValue={data.results.maxValue}
+                tileSpacing={0.01}
+                onClickTile={handleTileClick}
+                renderTooltip={(d) => {
+                  return `${d.col} - ${d.row}: ${d.value.toFixed(4)}`
+                }}
               />
-              <AutosizedGeneResultsManhattanPlot results={data.results.data} />
-            </>
-          )
-        }}
-      </Fetch>
-      <Fetch path={apiPath} params>
-        {({ data, error, loading }) => {
-          if (loading) {
-            return <StatusMessage>Loading heatmap...</StatusMessage>
-          }
-
-          if (error || !(data || {}).results) {
-            return <StatusMessage>Unable to load heatmap</StatusMessage>
-          }
-
-          return (
-            <AutosizedGeneResultsHeatmap
-              id="gene-results-heatmap"
-              height={1200}
-              columnLabels={data.results.columnLabels}
-              rowLabels={data.results.rowLabels}
-              data={data.results.data}
-              minValue={data.results.minValue}
-              maxValue={data.results.maxValue}
-              tileSpacing={0.01}
-              onTileClick={() => {}}
-              onTileHover={() => {}}
-              renderTooltip={(d) => {
-                return `${d.col} - ${d.row}: ${d.value.toFixed(4)}`
-              }}
-            />
-          )
-        }}
-      </Fetch>
+            )
+          }}
+        </Fetch>
+      </div>
     </>
   )
 }
