04431ca26e75849975e72fab028e1f11c70dec7a
daniaki <7043686+daniaki@users.noreply.github.com>
2021-08-15 14:00:50 +1000

    Added method to fetch gene records

--- src/browsers/tob/api/ApiClient.js
+++ src/browsers/tob/api/ApiClient.js
@@ -1,6 +1,3 @@
-import { GeneCellAssociation } from './data_types/GeneCallAssociation'
-import { VariantAssociation } from './data_types/VariantAssociation'
-
 export class ApiClient {
   constructor(endpoint = 'api/') {
     this.endpoint = endpoint
@@ -26,17 +23,17 @@ export class ApiClient {
   }
 
   /**
-   * @param {{ search: string, pValMax?: number }} options
-   * @returns {Promise<{data: GeneCellAssociation[], minValue: number, maxValue: number>}
+   * @param {{ search: string, threshold?: number }} options
+   * @returns {Promise<object>}
    */
-  fetchHeatmap({ search, pValMax }) {
+  fetchAssociations({ search, threshold }) {
     const params = new URLSearchParams()
 
     if (search.toString().trim()) {
       params.set('search', search.toString().trim())
     }
-    if (parseFloat(pValMax)) {
-      params.set('pValMax', pValMax)
+    if (parseFloat(threshold)) {
+      params.set('threshold', threshold)
     }
 
     // TODO:
@@ -44,42 +41,7 @@ export class ApiClient {
     //  2) Update heatmap component to accept callback to allowing how to get row name,
     //     col name and value
     //  3) Remove this data format translation.
-    return this.fetchApi(`/heatmap/?${params.toString()}`).then((result) => {
-      return {
-        ...result,
-        data: result.data.map((item) => {
-          return new GeneCellAssociation({
-            gene: item.row,
-            cell: item.col,
-            pval: item.value,
-          })
-        }),
-      }
-    })
-  }
-
-  /**
-   * @param {{ geneCellPairs: {gene: string, cell: string}[], pValMax?: number }} options
-   * @returns {Promise<{data: VariantAssociation[], minValue: number, maxValue: number>}
-   */
-  fetchLocusPlot({ geneCellPairs, pValMax }) {
-    const params = new URLSearchParams({ delimiter: '|' })
-
-    if (parseFloat(pValMax)) {
-      params.set('pValMax', pValMax)
-    }
-    if (geneCellPairs.length > 0) {
-      geneCellPairs.forEach(({ gene, cell }) => {
-        params.append('geneCellPair', [gene, cell].join('|'))
-      })
-    }
-
-    return this.fetchApi(`/locus-plot/?${params.toString()}`).then((result) => {
-      return {
-        ...result,
-        data: result.data.map((item) => new VariantAssociation({ ...item })),
-      }
-    })
+    return this.fetchApi(`/associations/?${params.toString()}`)
   }
 }
 
