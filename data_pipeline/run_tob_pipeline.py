2b9f5ce5d3785f30eac3b47e9a78e2d028deafa4
daniaki <7043686+daniaki@users.noreply.github.com>
2021-12-02 10:19:54 +1100

    added cmd arg to verify eQTL/eSNP processing

--- data_pipeline/run_tob_pipeline.py
+++ data_pipeline/run_tob_pipeline.py
@@ -1,4 +1,18 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import argparse
+
 from data_pipeline.datasets.tob.pipeline import run_pipeline
 
 if __name__ == "__main__":
-    run_pipeline()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--verify",
+        action="store_true",
+        help="Verify association ID uniqueness and table joins with eSNPs",
+    )
+
+    args = parser.parse_args()
+
+    run_pipeline(verify=args.verify)
