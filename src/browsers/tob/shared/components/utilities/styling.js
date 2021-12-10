cfc8200caa229f72e8eb40df7f03334cf3e91191
daniaki <7043686+daniaki@users.noreply.github.com>
2021-12-10 16:05:58 +1100

    Moved

---
+++ src/browsers/tob/shared/components/utilities/styling.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const TooltipWrapper = styled.div`
+  margin: 1em 0;
+`
+
+export const PlotWrapper = styled.div`
+  margin: 1em 0;
+  overflow: hidden;
+  width: 100%;
+`
+
+export default {
+  TooltipWrapper,
+  PlotWrapper,
+}
