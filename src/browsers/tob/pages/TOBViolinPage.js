bd1201db51814f680088f29c7073ad10b09f9ef3
daniaki <7043686+daniaki@users.noreply.github.com>
2022-02-07 14:06:46 +1100

    added match object to pass router parameters

--- src/browsers/tob/pages/TOBViolinPage.js
+++ src/browsers/tob/pages/TOBViolinPage.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import TOBViolinPlot from '../shared/TOBViolinPlot'
 
-const TOBViolinPage = () => {
-  return <TOBViolinPlot />
+const TOBViolinPage = ({ match }) => {
+  return <TOBViolinPlot gene={match.params.gene} />
 }
 
 TOBViolinPage.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
-  // match: PropTypes.object.isRequired,
+  match: PropTypes.object.isRequired,
 }
 
 export default TOBViolinPage
