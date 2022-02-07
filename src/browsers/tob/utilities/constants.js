4fecf9eef61f45b1b54de778cd2ebb5041bf2a3b
daniaki <7043686+daniaki@users.noreply.github.com>
2022-02-07 14:08:06 +1100

    default colors

---
+++ src/browsers/tob/utilities/constants.js
@@ -0,0 +1,20 @@
+export const defaultCellTypeColors = () => ({
+  bin: '#332288',
+  bmem: '#6699cc',
+  cd4et: '#88ccee',
+  cd4nc: '#44aa99',
+  cd4sox4: '#117733',
+  cd8nc: '#999933',
+  cd8et: '#ddcc77',
+  cd8s100b: '#661100',
+  plasma: '#cc6677',
+  dc: '#aa4466',
+  nk: '#882255',
+  nkr: '#aa4499',
+  monoc: '#1e1e1e',
+  mononc: '#cc0000',
+})
+
+export default {
+  defaultCellTypeColors,
+}
