4f806c13fc85da06199f954754c2bdf3a11b5551
daniaki <7043686+daniaki@users.noreply.github.com>
2022-02-08 12:48:47 +1100

    Convert gene to region before loading components

--- src/browsers/tob/pages/TOBAssociationPage.js
+++ src/browsers/tob/pages/TOBAssociationPage.js
@@ -1,12 +1,40 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 
 import TOBAssociationHeatmap from '../shared/TOBAssociationHeatmap'
 import TOBLocusZoomPlot from '../shared/TOBLocusZoomPlot'
+import { isEnsemblGeneId } from '../../../server/identifiers'
+import StatusMessage from '../../base/StatusMessage'
 
 const TOBAssociationPage = ({ match }) => {
   const [search, setSearch] = useState(match.params.query || '22:37200000-39900000')
+  const [query, setQuery] = useState(null)
+  const [error, setError] = useState(null)
   const [selectedTiles, setSelectedTiles] = useState(new Map())
+
+  useEffect(() => {
+    setQuery(null)
+
+    if (isEnsemblGeneId(search)) {
+      fetch(`/api/genes/${search}`, { method: 'GET' })
+        .then((r) => {
+          if (r.ok) {
+            r.json().then(
+              ({ results: { chrom, start, stop } }) => {
+                setQuery(`${chrom}:${start - 1e3}-${stop + 1e3}`)
+                setError(null)
+              },
+              () => setError('Could not parse result')
+            )
+          } else {
+            setError(`${r.status}: ${r.statusText}`)
+          }
+        })
+        .catch((e) => setError(e.toString()))
+    } else {
+      setQuery(search)
+    }
+  }, [search])
 
   const updateSelectedTiles = useCallback(
     (tile) => {
@@ -34,16 +62,24 @@ const TOBAssociationPage = ({ match }) => {
     [selectedTiles]
   )
 
+  if (error) {
+    return <StatusMessage>{error}</StatusMessage>
+  }
+
+  if (query == null) {
+    return <StatusMessage>Fetching region</StatusMessage>
+  }
+
   return (
     <>
       <TOBLocusZoomPlot
-        query={search}
+        query={query}
         genes={Array.from(selectedTiles.values()).map((t) => t.geneName)}
         cellTypes={Array.from(selectedTiles.values()).map((t) => t.cellTypeId)}
         onChange={(r) => setSearch(`${r.chrom}-${r.start}-${r.stop}`)}
       />
       <TOBAssociationHeatmap
-        query={search}
+        query={query}
         selectedTiles={Array.from(selectedTiles.values())}
         onChange={updateSelectedTiles}
       />
