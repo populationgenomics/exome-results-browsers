6fb522170fd98b7c3b43a4eb91e3413d1a84c78b
daniaki <7043686+daniaki@users.noreply.github.com>
2022-02-08 12:40:44 +1100

    Updated div placeholder text

--- src/browsers/tob/pages/TOBGenePage.js
+++ src/browsers/tob/pages/TOBGenePage.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const TOBGenePage = ({ match }) => <div>{match.params.gene}</div>
+const TOBGenePage = ({ match }) => <div>Gene page for {match.params.gene}</div>
 
 TOBGenePage.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
