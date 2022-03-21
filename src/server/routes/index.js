c582b50429fba382af7040af8ede44a01a5dc713
daniaki <7043686+daniaki@users.noreply.github.com>
2022-03-21 18:15:32 +1100

    Fix `toLowerCase` called on `undefined`

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -11,20 +11,21 @@ const genes = require('./genes')
 const errors = require('./errors')
 const old = require('./old')
 
+const { config } = require('../config')
+
 /**
  * @param {express.Express} app
- * @param {object} config
  */
-const setup = (app, config) => {
+const setup = (app) => {
   information.setup(app)
 
-  if (process.env.ENABLE_NEW_API.toLowerCase() === 'true') {
+  if (config.enableNewApi) {
     variants.setup(app)
     associations.setup(app)
     cellTypes.setup(app)
     genes.setup(app)
   } else {
-    old.setup(app, config)
+    old.setup(app)
   }
 
   errors.setup(app)
