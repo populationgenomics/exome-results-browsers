a687303d648c84c450d24ce1f1accb711de43c68
daniaki <7043686+daniaki@users.noreply.github.com>
2022-03-21 17:05:16 +1100

    Moved API routes to `routes/` directory

--- src/server/tob.js
+++ src/server/tob.js
@@ -5,48 +5,12 @@ const compression = require('compression')
 const express = require('express')
 const morgan = require('morgan')
 const helmet = require('helmet')
+const bodyParser = require('body-parser')
 
 const { OAuth2Client } = require('google-auth-library')
 
-const {
-  isRegionId,
-  isVariantId,
-  isRsId,
-  parseRegionId,
-  parseVariantId,
-} = require('@gnomad/identifiers')
-
-const {
-  fetchGenes,
-  fetchGenesAssociatedWithVariant,
-  fetchGenesById,
-  fetchGeneIdSuggestions,
-  fetchAssociationHeatmap,
-  fetchVariantsInRegion,
-  fetchVariantsById,
-  fetchCellTypes,
-  fetchCellTypesById,
-  fetchGenesInRegion,
-  fetchGeneExpression,
-} = require('./queries')
-const { parseConditioningRound } = require('./queries/utilities')
-const { convertPositionToGlobalPosition } = require('./queries/genome')
-const { isGene, isEnsemblGeneId } = require('./identifiers')
-
-// ================================================================================================
-// Configuration
-// ================================================================================================
-
-const isDevelopment = process.env.NODE_ENV === 'development'
-
-const config = {
-  enableHttpsRedirect: JSON.parse(process.env.ENABLE_HTTPS_REDIRECT || 'false'),
-  port: process.env.PORT || 8000,
-  trustProxy: JSON.parse(process.env.TRUST_PROXY || 'false'),
-  iapAudience: process.env.IAP_AUDIENCE,
-  oAuthClient: new OAuth2Client(),
-  maxRegionSize: process.env.MAX_REGION || 2.5e6,
-}
+const routes = require('./routes')
+const { config } = require('./config')
 
 // ================================================================================================
 // Express app
@@ -55,15 +19,10 @@ const config = {
 const app = express()
 
 app.set('trust proxy', config.trustProxy)
-
 app.use(compression())
 app.use(helmet())
-
-// ================================================================================================
-// Logging
-// ================================================================================================
-
-app.use(morgan(isDevelopment ? 'dev' : 'combined'))
+app.use(bodyParser.json())
+app.use(morgan(config.isDevelopment ? 'dev' : 'combined'))
 
 // ================================================================================================
 // HTTP => HTTPS redirect
@@ -84,6 +43,8 @@ if (config.enableHttpsRedirect) {
 // ================================================================================================
 
 if (config.iapAudience) {
+  const oAuthClient = new OAuth2Client()
+
   // Verify JWT token from IAP authentication
   app.use('/', async (req, res, next) => {
     const token = req.header('X-Goog-IAP-JWT-Assertion') || req.header('x-goog-iap-jwt-assertion')
@@ -92,8 +53,8 @@ if (config.iapAudience) {
 
     try {
       // Verify the id_token, and access the claims.
-      const response = await config.oAuthClient.getIapPublicKeysAsync()
-      const ticket = await config.oAuthClient.verifySignedJwtWithCertsAsync(
+      const response = await oAuthClient.getIapPublicKeysAsync()
+      const ticket = await oAuthClient.verifySignedJwtWithCertsAsync(
         token.toString(),
         response.pubkeys,
         config.iapAudience,
@@ -113,29 +74,11 @@ if (config.iapAudience) {
 }
 
 // ================================================================================================
-// Gene search
-// ================================================================================================
-
-app.use('/api/search', (req, res) => {
-  if (!req.query.q) {
-    return res.status(400).json({ error: 'Query required' })
-  }
-
-  if (Array.isArray(req.query.q)) {
-    return res.status(400).json({ error: 'One query required' })
-  }
-
-  return fetchGeneIdSuggestions({ query: req.query.q }).then((data) =>
-    res.status(200).json({ results: data })
-  )
-})
-
-// ================================================================================================
 // Dataset
 // ================================================================================================
 
 // Store dataset on request object so other route handlers can use it.
-app.use('/', (req, res, next) => {
+app.use('/', (req, _, next) => {
   req.dataset = 'tob'
   next()
 })
@@ -156,176 +99,30 @@ app.use('/config.js', (req, res) => {
 })
 
 // ================================================================================================
-// Gene
+// Setup API routing
 // ================================================================================================
 
-app.get('/api/genes', (req, res) => {
-  const { query } = req.query
-
-  if (isRegionId(query)) {
-    const region = parseRegionId(query)
-
-    if (Math.abs(region.stop - region.start) > config.maxRegionSize) {
-      return res.status(400).json({ error: 'Data is not available for a region this large' })
-    }
-
-    return fetchGenesInRegion({ region })
-      .then((results) => res.status(200).json({ results: { genes: results, region } }))
-      .catch((error) => res.status(400).json({ error: error.message }))
-  }
-
-  if (isEnsemblGeneId(query)) {
-    return fetchGenesById({ ids: [query] })
-      .then((results) =>
-        res.status(200).json({
-          results: {
-            genes: results,
-            region: {
-              chrom: results[0].chrom,
-              start: results[0].global_start,
-              stop: results[0].global_stop,
-            },
-          },
-        })
-      )
-      .catch((error) => res.status(400).json({ error: error.message }))
-  }
-
-  if (isVariantId(query)) {
-    const variant = parseVariantId(query)
-    return fetchGenesAssociatedWithVariant({ variant })
-      .then((results) =>
-        res.status(200).json({
-          results: {
-            genes: results,
-            region: {
-              chrom: variant.chrom,
-              start: Math.min(...results.map((r) => r.start)),
-              stop: Math.max(...results.map((r) => r.stop)),
-            },
-          },
-        })
-      )
-      .catch((error) => res.status(400).json({ error: error.message }))
-  }
-
-  return fetchGenes({ query })
-    .then((results) =>
-      res.status(200).json({
-        results: {
-          genes: results,
-          region: null,
-        },
-      })
+// Return index.html for React Swagger UI
+app.get('/api', (req, res) => {
+  res.sendFile(
+    path.resolve(
+      __dirname,
+      'public',
+      config.isDevelopment ? req.dataset : req.dataset.toUpperCase(),
+      'index.html'
     )
-    .catch((error) => res.status(400).json({ error: error.message }))
+  )
 })
 
-app.get('/api/genes/:id', (req, res) => {
-  return fetchGenesById({ ids: [req.params.id] })
-    .then((results) => res.status(200).json({ results: results[0] }))
-    .catch((error) => res.status(400).json({ error: error.message }))
-})
-
-app.get('/api/genes/:gene/residuals', (req, res) => {
-  return fetchGeneExpression({
-    gene: req.params.gene,
-    cellTypesIds: req.query.cellTypeIds?.split(',') || [],
-    chroms: req.query.chroms?.split(',') || [],
-  })
-    .then((results) => res.status(200).json({ results }))
-    .catch((error) => res.status(400).json({ error: error.message }))
-})
-
-// ================================================================================================
-// Variants
-// ===============================================================================================
-app.get('/api/associations/', (req, res) => {
-  let round = 1
-  try {
-    round = parseConditioningRound(req.query.round)
-  } catch (error) {
-    return res.status(400).json({ error: error.message })
-  }
-
-  const region = convertPositionToGlobalPosition({ ...parseRegionId(req.query.region) })
-  const genes = req.query.genes?.trim()?.split(',') || []
-  const cellTypes = req.query.cellTypes?.trim()?.split(',') || []
-
-  if (Math.abs(region.stop - region.start) > config.maxRegionSize) {
-    return res.status(400).json({ error: 'Data is not available for a region this large' })
-  }
-
-  return fetchVariantsInRegion({ region, genes, cellTypes, round })
-    .then((results) => res.status(200).json({ results }))
-    .catch((error) => res.status(400).json({ error: error.message }))
-})
-
-app.get('/api/associations/aggregate', (req, res) => {
-  const { query } = req.query
-
-  if (!(isRegionId(query) || isVariantId(query) || isRsId(query) || isGene(query))) {
-    return res.status(400).json({ error: 'Query must be a gene, region, variant ID or rsid' })
-  }
-
-  if (isRegionId(query)) {
-    const region = parseRegionId(query)
-
-    if (Math.abs(region.stop - region.start) > config.maxRegionSize) {
-      return res.status(400).json({ error: 'Data is not available for a region this large' })
-    }
-  }
-
-  let round = 1
-  try {
-    round = parseConditioningRound(req.query.round)
-  } catch (error) {
-    return res.status(400).json({ error: error.message })
-  }
-
-  return fetchAssociationHeatmap({ query, round, aggregateBy: 'p_value' })
-    .then((data) => res.status(200).json({ results: data }))
-    .catch((error) => res.status(400).json({ error: error.message }))
-})
-
-app.get('/api/associations/:id', (req, res) => {
-  return fetchVariantsById({ ids: [req.params.id] })
-    .then((results) => res.status(200).json({ results }))
-    .catch((error) => res.status(400).json({ error: error.message }))
-})
-
-// ================================================================================================
-// Cell types
-// ================================================================================================
-app.get('/api/cell-types/', (req, res) => {
-  return fetchCellTypes()
-    .then((results) => res.status(200).json({ results }))
-    .catch((error) => res.status(400).json({ error: error.message }))
-})
-
-app.get('/api/cell-types/:id', (req, res) => {
-  return fetchCellTypesById({ ids: [req.params.id] })
-    .then((results) => res.status(200).json({ results }))
-    .catch((error) => res.status(400).json({ error: error.message }))
-})
-
-// ================================================================================================
-// API error handling
-// ================================================================================================
-
-// Return 404 for unknown API paths.
-app.use('/api', (request, response) => {
-  response.status(404).json({ error: 'not found' })
-})
+routes.setup(app, config)
 
 // ================================================================================================
 // Static files
 // ================================================================================================
-
 // Serve static files from the appropriate dataset's directory. Webpack creates browser directories
 // using uppercase characters when in production mode.
-app.use((req, res, next) => {
-  req.url = `/${isDevelopment ? req.dataset : req.dataset.toUpperCase()}${req.url}`
+app.use((req, _, next) => {
+  req.url = `/${config.isDevelopment ? req.dataset : req.dataset.toUpperCase()}${req.url}`
   next()
 }, express.static(path.join(__dirname, 'public')))
 
@@ -335,7 +132,7 @@ app.use((req, res) => {
     path.resolve(
       __dirname,
       'public',
-      isDevelopment ? req.dataset : req.dataset.toUpperCase(),
+      config.isDevelopment ? req.dataset : req.dataset.toUpperCase(),
       'index.html'
     )
   )
