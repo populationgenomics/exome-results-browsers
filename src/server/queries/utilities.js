f2cfdec5bc28823a20d1713543e11be55ecf9528
daniaki <7043686+daniaki@users.noreply.github.com>
2022-03-22 19:09:38 +1100

    added upcoming tables

--- src/server/queries/utilities.js
+++ src/server/queries/utilities.js
@@ -6,6 +6,8 @@ const { ReferenceGenome } = require('./genome')
 const tableIds = {
   association: 'association',
   expression: 'log_residual',
+  variant: 'variant',
+  genotype: 'genotype',
   cellType: 'cell_type',
   geneModel: 'gene_model',
   geneLookup: 'gene_lookup',
