6c66ed72ab8415f8d2c1d3ae3b07f33939458566
daniaki <7043686+daniaki@users.noreply.github.com>
2022-03-22 16:21:34 +1100

    custom errors

---
+++ src/server/errors.js
@@ -0,0 +1,28 @@
+class ApiError extends Error {
+  statusCode = null
+}
+
+class ServerError extends ApiError {
+  statusCode = 500
+}
+class BadRequest extends ApiError {
+  statusCode = 400
+}
+
+class NotFound extends ApiError {
+  statusCode = 404
+}
+
+class MissingParameter extends BadRequest {}
+class InvalidParameter extends BadRequest {}
+class InvalidQueryParameter extends BadRequest {}
+
+module.exports = {
+  ApiError,
+  ServerError,
+  BadRequest,
+  NotFound,
+  MissingParameter,
+  InvalidParameter,
+  InvalidQueryParameter,
+}
