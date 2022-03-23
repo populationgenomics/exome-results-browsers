ed9ef7db9873ff11c847805ec9e7f311b84b54a8
daniaki <7043686+daniaki@users.noreply.github.com>
2022-03-23 12:48:55 +1100

    Helpers for specifying query option enums

---
+++ src/server/queries/options.js
@@ -0,0 +1,27 @@
+class Options {
+  static choices = {}
+
+  static isValid(choice) {
+    return Array.from(Object.values(this.choices)).includes(choice)
+  }
+
+  static options() {
+    return Object.values(this.choices)
+  }
+
+  static toString() {
+    return this.options()
+      .map((o) => `'${o}'`)
+      .join(', ')
+  }
+}
+
+class ExpressionOptions extends Options {
+  static choices = {
+    log_residual: 'log_residual',
+    residual: 'residual',
+    log_cpm: 'log_cpm',
+  }
+}
+
+module.exports = { ExpressionOptions }
